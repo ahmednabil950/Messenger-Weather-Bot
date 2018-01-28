e4f59a8bb7bd20ca354a59a4eecce99e3b0f5eff
Ahmed <ahmednabil950@bitbucket.org>
2018-01-28 21:58:47 +0200

    Initial commit

--- multichat/settings.py
+++ multichat/settings.py
@@ -76,12 +76,24 @@ WSGI_APPLICATION = 'multichat.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'djangogirls',
+    'USER': 'name',
+    'PASSWORD': '',
+    'HOST': 'localhost',
+    'PORT': '',
     }
 }
 
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
