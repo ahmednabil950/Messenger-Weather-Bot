9100a0807a73131cf6f74d4dcdff99384358a539
Ahmed <ahmednabil950@bitbucket.org>
2018-01-28 22:30:47 +0200

    Initial commit

--- multichat/settings.py
+++ multichat/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'multichat.wsgi.application'
 
 DATABASES = {
     'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'ENGINE': 'django.db.backends.hobby-dev',
             'NAME': 'multichat',
             'USER': 'name',
             'PASSWORD': '',
