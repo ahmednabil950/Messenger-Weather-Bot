14b57c1469d3b4329c099a2e33296aec437a2ab7
Ahmed <ahmednabil950@bitbucket.org>
2018-01-31 22:33:05 +0200

    first commit

--- multichat/settings.py
+++ multichat/settings.py
@@ -77,16 +77,24 @@ WSGI_APPLICATION = 'multichat.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-	    'NAME': 'd4s9svhq3buqr0',
-	    'USER': 'judtuggtaiqqse',
-	    'PASSWORD': '417402ff6537d12c6d1a4d721ee80c971994ea85e466d7acc87cc338b97638dd',
-	    'HOST': 'ec2-54-235-240-126.compute-1.amazonaws.com',
-	    'PORT': '5432',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
