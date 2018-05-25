84e1eaea6397f18fee402f1e707081b9c19547ed
Ahmed <ahmednabil950@bitbucket.org>
2018-05-25 17:59:36 +0200

    Weather bot initial commit

--- weatherapp/views.py
+++ weatherapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 from utils.messenger import messenger
 from django.views.decorators.csrf import csrf_exempt
+import json
 
 @csrf_exempt
 def main_view(request):
