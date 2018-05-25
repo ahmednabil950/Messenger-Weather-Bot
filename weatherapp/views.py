4990e00ca57480f893e604f9170a31b17f2fb590
Ahmed <ahmednabil950@bitbucket.org>
2018-05-25 18:22:44 +0200

    Weather bot initial commit

--- weatherapp/views.py
+++ weatherapp/views.py
@@ -4,6 +4,7 @@ from utils.messenger import messenger
 from django.views.decorators.csrf import csrf_exempt
 import json
 
+
 @csrf_exempt
 def main_view(request):
     if request.method == 'GET':
@@ -16,45 +17,45 @@ def main_view(request):
 def bot_access_token(request):
     bot = messenger()
 
-    if request.method == 'GET': 
+    if request.method == 'GET':
         token_sent = request.GET.get("hub.verify_token")
-        if  bot.Verify_Token(token_sent):
+        if bot.Verify_Token(token_sent):
             return HttpResponse(request.GET.get("hub.challenge"))
     return HttpResponse()
 
 
 @csrf_exempt
 def bot_sender(request):
-    
+
     print(request)
-    
+
     bot = messenger()
-    
+
     if request.method == 'POST':
- 
+
         bot.ACCESS_TOKEN = "EAAdfJWb1xs8BAJ1us9xi678ZBEMVDVv8cMQtvAcppW6ZCdjlzlYOkhBNoSqyZCfjxbwoFnYejy98k39nIlKyI2gcDZAuz8v4BpKuFgugOVYaAgl272VmAj5E1ot0jByYTTcUfswAiaIeppjpTmItZA2YWF12xNMIAbbKuA1I5ZCwZDZD"
-        
+
         all_json = json.loads(request.body.decode('utf-8'))
         print("###### JSON FORMAT ######")
         print(all_json)
 
-        recipient_id = bot.Return_Reception_ID (all_json)
+        recipient_id = bot.Return_Reception_ID(all_json)
         json_status = bot.check_json_sent(all_json)
 
- 
-        if json_status == "text" :
- 
+        if json_status == "text":
+
             try:
                 content = ["Hello There !!"]
                 text = bot.Return_Received_Text(all_json)
                 text = str(text)
                 print("#### TEXT DATATYPE #####")
                 print(type(text))
-                req_json , req_status = bot.Sent_text_Msgs(content , "RESPONSE" ,recipient_id )
+                req_json, req_status = bot.Sent_text_Msgs(
+                    content, "RESPONSE", recipient_id)
                 print("####### TXT IS #####")
                 print(text)
 
             except requests.exceptions.Timeout:
-                print ("time out")
- 
-    return HttpResponse ("s")
+                print("time out")
+
+    return HttpResponse("s")
