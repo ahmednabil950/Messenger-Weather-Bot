e1a9796dce762858b32cae31ab213b961af68493
Ahmed <ahmednabil950@bitbucket.org>
2018-05-30 23:25:44 +0200

    quick reply button test

--- weatherapp/views.py
+++ weatherapp/views.py
@@ -37,15 +37,13 @@ def bot_sender(request):
 
         bot.ACCESS_TOKEN = "EAAdfJWb1xs8BAJ1us9xi678ZBEMVDVv8cMQtvAcppW6ZCdjlzlYOkhBNoSqyZCfjxbwoFnYejy98k39nIlKyI2gcDZAuz8v4BpKuFgugOVYaAgl272VmAj5E1ot0jByYTTcUfswAiaIeppjpTmItZA2YWF12xNMIAbbKuA1I5ZCwZDZD"
 
-        all_json = json.loads(request.body.decode('utf-8'))
         print("###### JSON FORMAT ######")
-        print(all_json)
+        all_json = json.loads(request.body.decode('utf-8'))
         recipient_id = bot.get_receptient_ID(all_json)
-        json_status = bot.check_json_sent(all_json)
+        print(all_json)
         print("###### JSON STATUS ######")
+        json_status = bot.check_json_sent(all_json)
         print(json_status)
-
-        messenger.quick_reply('text', ['text'], recipient_id)
 
         if json_status == "text":
 
