d76ba1abc396b2d3b224895525625e4e6eed318e
ahmednabil950 <ahmednabil950@bitbucket.org>
2018-06-02 00:55:32 +0200

    dialogflow integration

--- weatherapp/views.py
+++ weatherapp/views.py
@@ -12,13 +12,12 @@ import json
 
 @csrf_exempt
 def main_view(request):
+    print(request)
     if request.method == 'GET':
         return bot_access_token(request)
 
     if request.method == 'POST':
         return bot_sender(request)
-
-    print(request)
 
 
 def bot_access_token(request):
